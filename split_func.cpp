0a436a98c527fac35e5404a21378495d1e084eb0
kumarsiddharth <kumar.siddharth450@gmail.com>
2016-08-08 16:59:11 +0530

    added some utility functions

--- split_func.cpp
+++ split_func.cpp
@@ -72,6 +72,43 @@ double entropy(int width,int height,int atx, int aty)
 
 }
 
+double entropy_2(int size, double **block, int atx, int aty)
+{
+  int i,j;
+  double frequency[256];
+  double element[256];
+  double entrop = 0.0;
+  double number = size * size;
+
+  for(i=0;i<256 ;i++)
+    element[i]=0.0;
+
+  for(i=atx;i<atx + size;i++){
+    for(j=aty;j<aty + size;j++){
+      
+       // printf("%f\t",block[i][j] );
+       element[(int)(floor(block[i][j]))]++;
+
+       // printf("count = %f\t",element[(int)(floor(block[i][j]))]);
+
+    }
+     // printf("\n");
+  }
+    // printf("\n");
+
+  for(i=0;i<256 ;i++)
+    frequency[i]= element[i] / number;
+
+  for(i=0;i<256 ;i++)
+    if(frequency[i] > 0)
+      entrop += frequency[i] * log((double) (1.0/frequency[i]))/log(2.0);
+
+  return entrop;
+
+
+
+}
+
 
 double variance(int width,int height,int atx, int aty)
 {
@@ -140,12 +177,15 @@ double variance_2(int size, double **block, int atx, int aty)
   double mean2;
   double mean;
 
-  for(i=atx;i<atx + size;i++)
-  for(j=aty;j<aty + size;j++) {
-     sum2 += block[i][j] * block[i][j];
-     sum  += block[i][j];
+  for(i=atx;i<atx + size;i++){
+    for(j=aty;j<aty + size;j++){
+       // printf("%f\t",block[i][j]);
+       sum2 += block[i][j] * block[i][j];
+       sum  += block[i][j];
+    }
+    // printf("\n");
   }
-  
+  // printf("\n");
   mean2 = sum2 / (size *size);
   mean = sum / (size *size);
 
@@ -154,3 +194,38 @@ double variance_2(int size, double **block, int atx, int aty)
 }
 
 
+double variance_3(int x_size,int y_size, double **block, int atx, int aty)
+{
+  int i,j;
+  double sum2 = 0.0;
+  double sum = 0.0;
+  double mean2;
+  double mean;
+
+  for(i=aty;i<aty + y_size;i++){
+    for(j=atx;j<atx + x_size;j++){
+       // printf("%f\t",block[i][j]);
+       sum2 += block[i][j] * block[i][j];
+       sum  += block[i][j];
+    }
+    // printf("\n");
+  }
+  // printf("\n");
+  mean2 = sum2 / (x_size *y_size);
+  mean = sum / (x_size *y_size);
+
+  return mean2 - mean * mean;
+
+}
+
+
+int min_2(int s1, int s2)
+{
+  return (s1 < s2 ? s1 : s2);
+}
+
+
+int max_2(int s1, int s2)
+{
+  return (s1 > s2 ? s1 : s2);
+}
